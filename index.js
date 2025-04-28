8f16f8d57b6e6d6726990b5fd29c84caeeb335be
lslove10010 <96574438+lslove10010@users.noreply.github.com>
2025-04-29 01:45:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const net = require('net');
 const { Buffer } = require('buffer');
 const { exec, execSync } = require('child_process');
 const { WebSocket, createWebSocketStream } = require('ws');
-const UUID = process.env.UUID || 'de04add9-5c68-6bab-950c-08cd5320df33'; // 运行哪吒v1,在不同的平台需要改UUID,否则会被覆盖
-const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
-const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || '';             // v1的NZ_CLIENT_SECRET或v0的agent端口                
-const DOMAIN = process.env.DOMAIN || '1234.abc.com';       // 填写项目域名或已反代的域名，不带前缀，建议填已反代的域名
-const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
-const SUB_PATH = process.env.SUB_PATH || 'sub';            // 获取节点的订阅路径
-const NAME = process.env.NAME || 'Vls';                    // 节点名称
-const PORT = process.env.PORT || 3000;                     // http和ws服务端口
+const UUID = process.env.UUID || 'd2a481b7-11be-44b5-84e5-755665945957'; 
+const NEZHA_SERVER = process.env.NEZHA_SERVER || ''; 
+const NEZHA_PORT = process.env.NEZHA_PORT || '';   
+const NEZHA_KEY = process.env.NEZHA_KEY || '';              
+const DOMAIN = process.env.DOMAIN || ''; 
+const AUTO_ACCESS = process.env.AUTO_ACCESS || false;
+const SUB_PATH = process.env.SUB_PATH || 'websub';  
+const NAME = process.env.NAME || 'serverstorm-nodews-Vls'; 
+const PORT = process.env.PORT || 1062;  
 
 const metaInfo = execSync(
   'curl -s https://speed.cloudflare.com/meta | awk -F\\" \'{print $26"-"$18}\' | sed -e \'s/ /_/g\'',
@@ -24,11 +24,13 @@ const ISP = metaInfo.trim();
 const httpServer = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World\n');
+    res.end('Hello, World, web is serverstorm hosting\n');
   } else if (req.url === `/${SUB_PATH}`) {
-    const vlessURL = `vless://${UUID}@www.visa.com.tw:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#${NAME}-${ISP}`;
+    const vlessURL = `vless://${UUID}@www.wto.org:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#${NAME}-${ISP}`;
     
     const base64Content = Buffer.from(vlessURL).toString('base64');
+
+    fs.writeFileSync('web.txt', base64Content + '\n');
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(base64Content + '\n');
@@ -41,7 +43,6 @@ const httpServer = http.createServer((req, res) => {
 const wss = new WebSocket.Server({ server: httpServer });
 const uuid = UUID.replace(/-/g, "");
 wss.on('connection', ws => {
-  // console.log("Connected successfully");
   ws.once('message', msg => {
     const [VERSION] = msg;
     const id = msg.slice(1, 17);
@@ -52,7 +53,6 @@ wss.on('connection', ws => {
     const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') :
     (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) :
     (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));
-    // console.log(`Connection from ${host}:${port}`);
     ws.send(new Uint8Array([VERSION, 0]));
     const duplex = createWebSocketStream(ws);
     net.connect({ host, port }, function() {
@@ -82,7 +82,6 @@ const getDownloadUrl = () => {
 const downloadFile = async () => {
   try {
     const url = getDownloadUrl();
-    // console.log(`Start downloading file from ${url}`);
     const response = await axios({
       method: 'get',
       url: url,
@@ -118,6 +117,9 @@ const runnz = async () => {
     command = `nohup ./npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &`;
   } else if (NEZHA_SERVER && NEZHA_KEY) {
     if (!NEZHA_PORT) {
+      const port = NEZHA_SERVER.includes(':') ? NEZHA_SERVER.split(':').pop() : '';
+      const tlsPorts = new Set(['443', '8443', '2096', '2087', '2083', '2053']);
+      const nezhatls = tlsPorts.has(port) ? 'tls' : 'false';
       const configYaml = `
 client_secret: ${NEZHA_KEY}
 debug: false
@@ -134,7 +136,7 @@ server: ${NEZHA_SERVER}
 skip_connection_count: false
 skip_procs_count: false
 temperature: false
-tls: false
+tls: ${nezhatls}
 use_gitee_to_upgrade: false
 use_ipv6_country_code: false
 uuid: ${UUID}`;
@@ -190,5 +192,6 @@ httpServer.listen(PORT, () => {
     delFiles();
   }, 30000);
   addAccessTask();
-  console.log(`Server is running on port ${PORT}`);
+  const host = httpServer.address().address;
+  console.log(`Server is running on http://${host}:${PORT}`);
 });
